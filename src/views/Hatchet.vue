9deb7ba1d808d7868ab859d48d37d49c5ed41df8
Wyatt Nolen <wyattnolen@gmail.com>
2020-10-04 00:26:21 -0400

    Get percentages
    Console logs the occurances of each modifier in the deck.

---
+++ src/views/Hatchet.vue
@@ -0,0 +1,51 @@
+<template>
+  <article></article>
+</template>
+
+<script>
+// import PlayerBoard from "@/components/PlayerBoard.vue";
+
+export default {
+  name: "Hatchet",
+  components: {
+    // PlayerBoard
+  },
+  data: function() {
+    return {
+      deck: ["0", "1", "1"],
+      perks: [],
+      selectedPerks: [],
+    };
+  },
+  computed: {
+    getTotalAmount() {
+      return this.deck.length;
+    },
+    getUniqueValues() {
+      return [...new Set(this.deck)];
+    },
+  },
+  created() {
+    this.determinePercentages();
+  },
+  watch: {},
+  methods: {
+    determinePercentages() {
+      let values = this.getUniqueValues;
+      let total = this.getTotalAmount;
+      values.forEach((modifier) => {
+        const numItems = this.deck.filter((card) => card === modifier);
+        console.log(
+          `modifier ${modifier} represents ${(numItems.length * 100) / total}%`,
+        );
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+span {
+  font-weight: bold;
+}
+</style>
