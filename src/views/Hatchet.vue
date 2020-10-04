f8bfb1a476dd8567e71af3e5e7ff61324e19db68
Wyatt Nolen <wyattnolen@gmail.com>
2020-10-04 03:14:26 -0400

    Base concept for a bar graph display

--- src/views/Hatchet.vue
+++ src/views/Hatchet.vue
@@ -1,9 +1,30 @@
 <template>
   <article>
     {{ deck }}
-    <section class="" v-for="data in getPercentages" :key="data">
-      <p>Modifer {{ data.modifier }}</p>
-      <p>Percentage {{ data.percent }}</p>
+    <section class="graph">
+      <div class="graph__inner">
+        <div
+          v-for="data in getPercentages"
+          :key="data"
+          class="graph__bar"
+          :style="{ '--percent': data.percent + '%' }"
+        >
+          <p>Modifer {{ data.modifier }}</p>
+          <p>Percentage {{ data.percent }}</p>
+        </div>
+      </div>
+      <ul class="graph__axis graph__axis--y">
+        <li>100%</li>
+        <li>75%</li>
+        <li>50%</li>
+        <li>25%</li>
+        <li>0%</li>
+      </ul>
+      <ul class="graph__axis graph__axis--x">
+        <li v-for="modifier in getUniqueValues" :key="modifier">
+          {{ modifier }}
+        </li>
+      </ul>
     </section>
     <p @click="test()">test</p>
   </article>
@@ -19,7 +40,7 @@ export default {
   },
   data: function() {
     return {
-      deck: ["0", "1", "1"],
+      deck: [0, 1, 1],
       perks: [],
       selectedPerks: [],
     };
@@ -56,5 +77,44 @@ export default {
 <style lang="scss">
 span {
   font-weight: bold;
+}
+.graph {
+  display: grid;
+  grid-template-areas:
+    "y chart"
+    ".  x";
+  width: 80vw;
+  height: 80vh;
+  &__axis {
+    display: flex;
+    padding: 0;
+    margin: 0;
+    &--y {
+      flex-direction: column;
+      justify-content: space-between;
+      grid-area: y;
+    }
+    &--x {
+      flex-direction: row;
+      justify-content: space-around;
+      grid-area: x;
+    }
+    > li {
+      list-style: none;
+    }
+  }
+  &__inner {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: flex-end;
+    border-left: 1px solid #000;
+    border-bottom: 1px solid #000;
+    grid-area: chart;
+  }
+  &__bar {
+    height: var(--percent);
+    background-color: green;
+  }
 }
 </style>
