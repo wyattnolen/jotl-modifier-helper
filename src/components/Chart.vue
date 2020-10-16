e92bbe87ad9b174062c9b965118105a8a48dc66f
Wyatt Nolen <wyattnolen@gmail.com>
2020-10-15 21:13:07 -0400

    Update

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -9,7 +9,7 @@
         :class="[
           data.type == 'positive'
             ? 'chart__bar--positive'
-            : 'chart__bar--negative',
+            : 'chart__bar--negative'
         ]"
         :style="{ '--percent': data.percent + '%' }"
       >
@@ -30,191 +30,193 @@
 </template>
 
 <script>
-export default {
-  name: "Perks",
-  props: {
-    deck: Array,
-    deckLength: Number,
-    uniqueModifiers: Array,
-  },
-  data: function() {
-    return {};
-  },
-  computed: {
-    getChartData() {
-      let chartData = [];
-      this.uniqueModifiers.forEach((modifier) => {
-        let numItems = this.deck.filter((card) => card === modifier);
-        let percentage = (numItems.length * 100) / this.deckLength;
-        chartData.push({
-          modifier: modifier,
-          percent: parseFloat(percentage.toFixed(2)),
-          type: this.determineType(modifier),
+  export default {
+    name: "Perks",
+    props: {
+      deck: Array,
+      deckLength: Number,
+      uniqueModifiers: Array
+    },
+    data: function() {
+      return {};
+    },
+    computed: {
+      getChartData() {
+        let chartData = [];
+        this.uniqueModifiers.forEach(modifier => {
+          let numItems = this.deck.filter(card => card === modifier);
+          let percentage = (numItems.length * 100) / this.deckLength;
+          chartData.push({
+            modifier: modifier,
+            percent: parseFloat(percentage.toFixed(2)),
+            type: this.determineType(modifier)
+          });
         });
-      });
-      return chartData;
-    },
-  },
-  methods: {
-    determineType(modifier) {
-      let type = "";
-      if (modifier >= 0 || modifier == "x2") {
-        type = "positive";
-      } else if (modifier < 0 || modifier == "x0") {
-        type = "negative";
+        return chartData;
       }
-      return type;
     },
-  },
-};
+    methods: {
+      determineType(modifier) {
+        let type = "";
+        if (modifier >= 0 || modifier == "x2") {
+          type = "positive";
+        } else if (modifier < 0 || modifier == "x0") {
+          type = "negative";
+        }
+        return type;
+      }
+    }
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-@import "../scss/_shared.scss";
+  @import "../scss/_shared.scss";
 
-.chart {
-  display: grid;
-  grid-template-areas:
-    "title title"
-    "x chart"
-    ".  y";
-  grid-template-columns: 10% 90%;
-  grid-template-rows: 10% 80% 10%;
-  height: 100%;
-  @media (min-width: $sm) {
+  .chart {
+    display: grid;
     grid-template-areas:
       "title title"
-      "y chart"
-      ".  x";
+      "x chart"
+      ".  y";
     grid-template-columns: 10% 90%;
     grid-template-rows: 10% 80% 10%;
-    column-gap: 20px;
-  }
+    height: 100%;
+    @media (min-width: $sm) {
+      grid-template-areas:
+        "title title"
+        "y chart"
+        ".  x";
+      grid-template-columns: 10% 90%;
+      grid-template-rows: 10% 80% 10%;
+      column-gap: 20px;
+    }
 
-  &__title {
-    grid-area: title;
-  }
-  &__axis {
-    display: flex;
-    padding: 0;
-    margin: 0;
-    &--y {
-      flex-direction: row-reverse;
-      justify-content: space-between;
-      grid-area: y;
-      @media (min-width: $sm) {
-        flex-direction: column;
+    &__title {
+      grid-area: title;
+    }
+    &__axis {
+      display: flex;
+      padding: 0;
+      margin: 0;
+      &--y {
+        flex-direction: row-reverse;
         justify-content: space-between;
-        margin: 0;
-        line-height: 0;
-        align-items: flex-end;
-      }
-    > li {
-      flex-basis: 20%;
-      &:nth-child(1) {
-        text-align: right;
-      }
-      &:nth-child(2) {
-        text-align: right;
-      }
-      &:nth-child(3) {
-        text-align: center;
-      }
-      &:nth-child(4) {
-        text-align: left;
-      }
-      &:nth-child(5) {
-        text-align: left;
-      }
+        grid-area: y;
         @media (min-width: $sm) {
-          flex-basis: auto;
+          flex-direction: column;
+          justify-content: space-between;
           margin: 0;
           line-height: 0;
+          align-items: flex-end;
+        }
+        > li {
+          flex-basis: 20%;
+          &:nth-child(1) {
+            text-align: right;
+          }
+          &:nth-child(2) {
+            text-align: right;
+          }
+          &:nth-child(3) {
+            text-align: center;
+          }
+          &:nth-child(4) {
+            text-align: left;
+          }
+          &:nth-child(5) {
+            text-align: left;
+          }
+          @media (min-width: $sm) {
+            flex-basis: auto;
+            margin: 0;
+            line-height: 0;
+          }
         }
       }
-    }
-    &--x {
-      flex-direction: column;
-      justify-content: space-between;
-      grid-area: x;
-      @media (min-width: $sm) {
-        flex-direction: row;
-        justify-content: space-around;
+      &--x {
+        flex-direction: column;
+        justify-content: space-between;
+        grid-area: x;
+        @media (min-width: $sm) {
+          flex-direction: row;
+          justify-content: space-around;
+        }
+
+        > li {
+          margin: 10px 0;
+          @media (min-width: $sm) {
+            margin: 0;
+          }
+        }
       }
 
       > li {
-        margin: 10px 0;
-        @media (min-width: $sm) {
-          margin: 0;
-        }
+        list-style: none;
       }
     }
-
-    > li {
-      list-style: none;
-    }
-  }
-  &__inner {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: flex-start;
-        background: repeating-linear-gradient(
-          to right,
-      transparent 24.5%,
-      transparent 49.5%,
-      $pri 50%,
-      $pri 50%);
-        @media (min-width: $sm) {
-              background: repeating-linear-gradient(
-      transparent 24.5%,
-      transparent 49.5%,
-      $pri 50%,
-      $pri 50%);
-        }
-
-    border-left: 1px solid $pri;
-    border-bottom: 1px solid $pri;
-    grid-area: chart;
-
-    @media (min-width: $sm) {
-      flex-direction: row;
-      align-items: flex-end;
-    }
-  }
-  &__bar {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-end;
-    width: calc(var(--percent) * (100 / (var(--ay) * 25)));
-    min-height: 40px;
-    margin: 10px 0;
-
-    @media (min-width: $sm) {
-      justify-content: flex-start;
-      align-items: center;
-      width: auto;
-      height: calc(var(--percent) * (100 / (var(--ay) * 25)));
-      min-width: 40px;
-      margin: 0;
-    }
-
-    &--positive {
-      background-color: $positive;
-    }
-    &--negative {
-      background-color: $negative;
-    }
-    > p {
-      margin: 0;
-      margin-right: -30px;
+    &__inner {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+      align-items: flex-start;
+      background: repeating-linear-gradient(
+        to right,
+        transparent 24.5%,
+        transparent 49.5%,
+        var(--pri) 25%,
+        var(--pri) 50%
+      );
       @media (min-width: $sm) {
-        margin-right: 0;
-        margin-top: -30px;
+        background: repeating-linear-gradient(
+          transparent 24.5%,
+          transparent 49.5%,
+          var(--pri) 25%,
+          var(--pri) 50%
+        );
+      }
+
+      border-left: 1px solid var(--pri);
+      border-bottom: 1px solid var(--pri);
+      grid-area: chart;
+
+      @media (min-width: $sm) {
+        flex-direction: row;
+        align-items: flex-end;
+      }
+    }
+    &__bar {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: flex-end;
+      width: calc(var(--percent) * (100 / (var(--ay) * 25)));
+      min-height: 40px;
+      margin: 10px 0;
+
+      @media (min-width: $sm) {
+        justify-content: flex-start;
+        align-items: center;
+        width: auto;
+        height: calc(var(--percent) * (100 / (var(--ay) * 25)));
+        min-width: 40px;
+        margin: 0;
+      }
+
+      &--positive {
+        background-color: $positive;
+      }
+      &--negative {
+        background-color: $negative;
+      }
+      > p {
+        margin: 0;
+        margin-right: -30px;
+        @media (min-width: $sm) {
+          margin-right: 0;
+          margin-top: -30px;
+        }
       }
     }
   }
-}
 </style>
--- src/views/Hatchet.vue
+++ src/views/Hatchet.vue
@@ -1,125 +1,160 @@
 <template>
   <article class="dashboard">
-    <section class="dashboard__a">
-      <Snapshots v-bind:deck="deck" v-bind:deckLength="getTotalAmount">
-      </Snapshots>
-    </section>
+    <div class="dashboard__inner">
+      <section class="dashboard__a">
+        <Snapshots v-bind:deck="deck" v-bind:deckLength="getTotalAmount">
+        </Snapshots>
+      </section>
 
-    <section class="dashboard__b">
-      <Character></Character>
-      <Perks
-        v-bind:deck="deck"
-        @add-to-deck="addToDeck"
-        @remove-from-deck="removeFromDeck"
-      ></Perks>
-    </section>
+      <section class="dashboard__b">
+        <Character></Character>
+        <Perks
+          v-bind:deck="deck"
+          @add-to-deck="addToDeck"
+          @remove-from-deck="removeFromDeck"
+        ></Perks>
+      </section>
 
-    <section class="dashboard__c">
-      <Chart
-        v-bind:deck="deck"
-        v-bind:uniqueModifiers="getUniqueModifiers"
-        v-bind:deckLength="getTotalAmount"
-      ></Chart>
-    </section>
+      <section class="dashboard__c">
+        <Chart
+          v-bind:deck="deck"
+          v-bind:uniqueModifiers="getUniqueModifiers"
+          v-bind:deckLength="getTotalAmount"
+        ></Chart>
+      </section>
+    </div>
   </article>
 </template>
 
 <script>
-import Snapshots from "@/components/Snapshots.vue";
-import Character from "@/components/Character.vue";
-import Perks from "@/components/Perks.vue";
-import Chart from "@/components/Chart.vue";
+  import Snapshots from "@/components/Snapshots.vue";
+  import Character from "@/components/Character.vue";
+  import Perks from "@/components/Perks.vue";
+  import Chart from "@/components/Chart.vue";
 
-export default {
-  name: "Hatchet",
-  components: {
-    Snapshots,
-    Character,
-    Perks,
-    Chart,
-  },
-  data: function() {
-    return {
-      deck: [
-        "x0",
-        "-2",
-        "-1",
-        "-1",
-        "-1",
-        "-1",
-        "-1",
-        "+0",
-        "+0",
-        "+0",
-        "+0",
-        "+0",
-        "+0",
-        "+1",
-        "+1",
-        "+1",
-        "+1",
-        "+2",
-        "x2",
-      ],
-      specialEffects: [],
-      characters: [],
-      selectedCharacter: "",
-    };
-  },
-  computed: {
-    getTotalAmount() {
-      return this.deck.length + 1;
+  export default {
+    name: "Hatchet",
+    components: {
+      Snapshots,
+      Character,
+      Perks,
+      Chart
     },
-    getUniqueModifiers() {
-      return [...new Set(this.deck)];
+    data: function() {
+      return {
+        deck: [
+          "x0",
+          "-2",
+          "-1",
+          "-1",
+          "-1",
+          "-1",
+          "-1",
+          "+0",
+          "+0",
+          "+0",
+          "+0",
+          "+0",
+          "+0",
+          "+1",
+          "+1",
+          "+1",
+          "+1",
+          "+2",
+          "x2"
+        ],
+        specialEffects: [],
+        characters: [],
+        selectedCharacter: ""
+      };
     },
-  },
-  created() {
-    // this.determinePercentages();
-  },
-  watch: {},
-  methods: {
-    addToDeck(modifier) {
-      this.deck.push(modifier);
+    computed: {
+      getTotalAmount() {
+        return this.deck.length + 1;
+      },
+      getUniqueModifiers() {
+        return [...new Set(this.deck)];
+      }
     },
-    removeFromDeck(index) {
-      this.deck.splice(index, 1);
+    created() {
+      // this.determinePercentages();
     },
-  },
-};
+    watch: {},
+    methods: {
+      addToDeck(modifier) {
+        this.deck.push(modifier);
+      },
+      removeFromDeck(index) {
+        this.deck.splice(index, 1);
+      }
+    }
+  };
 </script>
 
 <style lang="scss">
-@import "../scss/_shared.scss";
+  @import "../scss/_shared.scss";
 
-.dashboard {
-  display: grid;
-  grid-template-areas:
-    "b"
-    "a"
-    "c";
-  grid-template-columns: 100%;
-  gap: 30px 30px;
-  max-width: 90vw;
-  margin: 0 auto;
-  @media (min-width: $sm) {
-    grid-template-areas:
-      "b a"
-      "b c";
-    grid-template-columns: 30% 70%;
-    grid-template-rows: 0fr 1fr;
-    max-width: 90vw;
+  .dashboard {
+    /* Default Color */
+    --pri: #e9eaef;
+    --alt: #ffffff;
+    --textColor: #333;
+    --titleColor: #000;
+
+    max-width: 100%;
+    min-height: 100vh;
+    height: 100%;
+    padding: 30px 5vw;
+    margin: 0 auto;
+    background-color: var(--pri);
+
+    &.hatchet {
+      --pri: #aba194;
+      --alt: #ddd1c7;
+    }
+    &.demolitionist {
+      --pri: #aba194;
+      --alt: #ddd1c7;
+    }
+    &.redguard {
+      --pri: #aba194;
+      --alt: #ddd1c7;
+    }
+    &.voidwarden {
+      --pri: #1a1c2a;
+      --alt: #222437;
+      --textColor: #a5a6ad;
+      --titleColor: #fff;
+    }
+
+    &__inner {
+      display: grid;
+      grid-template-areas:
+        "b"
+        "a"
+        "c";
+      grid-template-columns: 100%;
+      gap: 30px 30px;
+
+      @media (min-width: $sm) {
+        grid-template-areas:
+          "b a"
+          "b c";
+        grid-template-columns: 30% 70%;
+        grid-template-rows: 0fr 1fr;
+      }
+    }
+
+    &__a {
+      grid-area: a;
+    }
+    &__b {
+      grid-area: b;
+      @include card;
+    }
+    &__c {
+      grid-area: c;
+      @include card;
+    }
   }
-  &__a {
-    grid-area: a;
-  }
-  &__b {
-    grid-area: b;
-    @include card;
-  }
-  &__c {
-    grid-area: c;
-    @include card;
-  }
-}
 </style>
